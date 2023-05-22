83d5f6ffc989a6eca1e21cb74cb09c757b821489
laconis <laconis.dev@gmail.com>
2023-05-22 15:02:20 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+static function carre($x){return x*x;}
+static function cube($x){return x*x*x;}
+
+//Function // 
+static function equals($sha1,$sha2){
+  if($sha1 == $sha2){
+    return true;
+  }
+   return false;
+}
