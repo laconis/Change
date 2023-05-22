3db6335067a18aa4e9aa128aa73add3a0fd4623a
laconis <laconis.dev@gmail.com>
2023-05-22 15:28:18 +0200

    Update functions.php
    Add sql function

--- functions.php
+++ functions.php
@@ -11,11 +11,25 @@ static function equals($sha1,$sha2){
    return false;
 }
 
-static function count_sql_rows(){
+static function count_sql_rows($field,$table){
  $conn = new Sql_Core_Laconis(); 
 $sql = "Select  count($field) as nb from $table";
-  
   $res1 = $conn->sql_select_one_data($sql);
+  return $res1['nb'];
+}
+
+
+static function count_sql_colums($table){
+ $conn = new Sql_Core_Laconis(); 
+$sql = "Show columns from $table";
+  $res1 = $conn->sql_select_one_data($sql);
+  return $res1;
+}
+
+static function statGroup(){
+    $sql = 
+  
   
 }
+
 
